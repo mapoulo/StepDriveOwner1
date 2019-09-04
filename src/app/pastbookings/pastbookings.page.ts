766db6b54f0ad98b2fdf3445013cca42423d0f05
NkweJustice <oupamapoulo@gmail.com>
2019-09-04 08:28:54 +0200

    changes on requests page made

--- src/app/pastbookings/pastbookings.page.ts
+++ src/app/pastbookings/pastbookings.page.ts
@@ -6,19 +6,40 @@ import * as firebase from 'firebase';
   styleUrls: ['./pastbookings.page.scss'],
 })
 export class PastbookingsPage implements OnInit {
+  
   db = firebase.firestore();
-  storage = firebase.storage().ref();
+  storage = firebase.firestore();
   requests=[];
+  NewRequeste = [];
   constructor() { 
-    this.db.collection('request').onSnapshot(snapshot => {
+    // this.db.collection('request').onSnapshot(snapshot => {
+    //   snapshot.forEach(doc => {
+    //     // this.users = doc.data();
+    //     this.requests.push(doc.data());
+    //     console.log('The requests:',this.requests);
+    //   })
+    // });
+
+    this.storage.collection('request').onSnapshot(snapshot => {
       snapshot.forEach(doc => {
         // this.users = doc.data();
+        // this.addMarkersOnTheCustomersCurrentLocation(this.users.coords.lat, this.users.coords.lng);
         this.requests.push(doc.data());
-        console.log('The requests:',this.requests);
+        console.log('My array is ',this.requests);
         
-        
+
+        this.requests.forEach(Customers => {
+          console.log('Customers in my array', Customers.schooluid);
+          console.log('Owners UID logged in', firebase.auth().currentUser.uid);
+          if(Customers.schooluid === firebase.auth().currentUser.uid){
+             this.NewRequeste.push(doc.data())
+          }
+        }) 
+
       })
     });
+    
+console.log("Data in my New Array is:", this.NewRequeste);
 
 
   }
